f5c3a9921b5cc636f4bf05d02c0850232bd7aaeb
Austin Alber <austinalber@gmail.com>
2019-08-19 04:18:19 -0400

    Added connection variable to connect to MySQL

--- bamazon.js
+++ bamazon.js
@@ -14,5 +14,25 @@ const mysql = require('mysql');
 // Functions
 
 
-// Run Bamazon
+// Connect to MySQL
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Gamecube1",
+    database: "products_dq"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
 
+    // Run Bamazon function
+    
+  });
