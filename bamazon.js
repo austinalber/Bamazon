0404a6f4ceec34785f53de9c6964cfb87df42b01
Austin Alber <austinalber@gmail.com>
2019-08-19 03:56:26 -0400

    Added initial bamazon.js file

---
+++ bamazon.js
@@ -0,0 +1,18 @@
+// Required NPMs
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+
+// Prompts for inquirer
+
+
+// User Input
+
+
+// Variables
+
+
+// Functions
+
+
+// Run Bamazon
+
